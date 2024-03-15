8136048373240ead82d2e619b34fd35825ef5119
Aneeta <aneetajohn@gmail.com>
2024-03-15 10:00:08 -0400

    Changes made in reading JSON file according to userstory

--- src/main/java/com/oasis/poc1/entity/OutputJson.java
+++ src/main/java/com/oasis/poc1/entity/OutputJson.java
@@ -1,26 +1,58 @@
 package com.oasis.poc1.entity;
 
-import java.util.Arrays;
+import java.util.Date;
 
 public class OutputJson {
+		
+	private String entityName;
+	private String label;
+	private String controlType;
+	private String attribute;
+	private String value;	
+	private Date timestamp;
 	
-	private OutputJsonControl[] data;
-
-	public OutputJsonControl[] getData() {
-		return data;
+	
+	public String getEntityName() {
+		return entityName;
 	}
-
-	public void setData(OutputJsonControl[] data) {
-		this.data = data;
+	public void setEntityName(String entityName) {
+		this.entityName = entityName;
 	}
-
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public String getControlType() {
+		return controlType;
+	}
+	public void setControlType(String controlType) {
+		this.controlType = controlType;
+	}
+	public String getAttribute() {
+		return attribute;
+	}
+	public void setAttribute(String attribute) {
+		this.attribute = attribute;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+	
 	@Override
 	public String toString() {
-		return "OutputJson [data=" + Arrays.toString(data) + "]";
-	}
-	
-	
-	
-	
+		return "OutputJson [entityName=" + entityName + ", label=" + label + ", controlType=" + controlType
+				+ ", attribute=" + attribute + ", value=" + value + ", timestamp=" + timestamp + "]";
+	}	
 
 }
